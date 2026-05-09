45463afef58421185adffa6d81b764fb20c2393e
MarlonX-a <marlonalvime05@gmail.com>
2026-05-09 10:18:33 -0500

    Primeros cambios

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,24 @@
+import React from 'react'
+
+export function Navbar() {
+  return (
+    <>
+        <nav className="navbar">
+            <div className="nav-container">
+                <h2 className="nav-logo">AuditIA</h2>
+                <ul className="nav-menu">
+                    <li className="nav-item">
+                        <a href="/" className="nav-link">Home</a>
+                    </li>
+                    <li className="nav-item">
+                        <a href="/about" className="nav-link">About</a>
+                    </li>
+                    <li className="nav-item">
+                        <a href="/contact" className="nav-link">Contact</a>
+                    </li>
+                </ul>
+            </div>
+        </nav>
+    </>
+  )
+}
---
+++ src/pages/HomePage.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export function Homepage() {
+  return (
+    <>
+        <h1>Hola mundo perras</h1>
+    </>
+  )
+}
